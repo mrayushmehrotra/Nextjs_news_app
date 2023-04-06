4893d2f9066a801c03c04f72f97bceba7f358888
Your Name <you@example.com>
2023-04-06 20:42:16 +0530

    Statrted a news app

---
+++ src/pages/eom.js
@@ -0,0 +1,14 @@
+export const EOM = () =>{
+
+    return(
+        <div className="page-container">
+            <div>
+                <h1>Developer</h1>
+            </div>
+        </div>
+    );
+};
+
+
+
+export default EOM;
