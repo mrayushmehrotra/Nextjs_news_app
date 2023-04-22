2340979bf429ebb4e5c2b6351508daa3793de774
Mayush <110099419+Ayushoopp@users.noreply.github.com>
2023-04-22 11:41:45 +0530

    Update eom.js

--- src/pages/eom.js
+++ src/pages/eom.js
@@ -12,7 +12,7 @@ export const EOM = ({}) =>{
                 <h1>Developer</h1>
                 <h2>Ayush Mehrotra</h2>
                 <h3>Web2 & Web3 Developer</h3>
-                <img style={{height: "300px", width: "300px;"}} src="https://pbs.twimg.com/profile_images/1576103656914817024/wWNNbpEM_400x400.jpg" />
+                <img style={{height: "300px", width: "300px;"}} src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQxaKNZTvrRu8Kx4pex2mLzx4xk07z0_5j3pg&usqp=CAU"/>
                 </div>
             
         </div>
