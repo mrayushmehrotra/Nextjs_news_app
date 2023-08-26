86e711026476c078332e38433717cb3eb500f4cc
ayushM <110099419+mrayushmehrotra@users.noreply.github.com>
2023-08-26 14:55:25 +0530

    Update eom.js

--- src/pages/eom.js
+++ src/pages/eom.js
@@ -15,7 +15,7 @@ export const EOM = ({}) =>{
                 <h1>Developer</h1>
                 <h2>Ayush Mehrotra</h2>
                 <h3>Web Developer</h3>
-                <img style={{height: "300px", width: "300px;"}} src='https://media.licdn.com/dms/image/D4D03AQHZkFHuDnlT1Q/profile-displayphoto-shrink_400_400/0/1689145787591?e=1695254400&v=beta&t=pG4Njk7efmCx9zK4fwEVuLSD0Jw_xZn5G8WS3ZO51_M'/>
+                <img style={{height: "300px", width: "300px;"}} src='https://twitter.com/AYUSH070707/photo'/>
                 </div>
             
         </div>
